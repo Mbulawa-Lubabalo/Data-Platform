e6dc1dcf045621fa92e377ce8cfdb37097888df9
Lubabalo Mbulawa <lmbulawa4@gmail.com>
2025-12-30 15:35:08 +0200

    tests for extracting data

--- Data_Ingestion/extract.py
+++ Data_Ingestion/extract.py
@@ -3,21 +3,8 @@ import requests
 url = "https://isibaloweb.statssa.gov.za/data/ETS/Monthly/Export%20and%20Import%20Unit%20Value%20IndicesP0142_7/P0142_7p.json"
     
 def Fetch_Data():
-
-    # url = "https://isibaloweb.statssa.gov.za/data/ETS/Monthly/Export%20and%20Import%20Unit%20Value%20IndicesP0142_7/P0142_7p.json"
-    
-    # headers = {
-    # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-    #               "AppleWebKit/537.36 (KHTML, like Gecko) "
-    #               "Chrome/120.0.0.0 Safari/537.36",
-    # "Accept": "application/json, text/plain, */*",
-    # "Referer": "https://isibaloweb.statssa.gov.za/",
-    # "Origin": "https://isibaloweb.statssa.gov.za",
-    # }
     
     response = requests.get(url, timeout=10)
-    # print("Status code:", response)
-    # print("Raw response text:", response.text)
     response.raise_for_status()
     data = response.json()
     return data
